b65b0074a27ea6d5fc108db84a8ba2c38c79cbf4
ColinAdo <colinadodev30@gmail.com>
2024-02-17 11:20:47 +0300

    Add working with list. ScrollView and RefreshControl

--- App.js
+++ App.js
@@ -1,50 +1,60 @@
 import { useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, Linking, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, RefreshControl } from 'react-native';
 
 export default function App() {
 
+  const [Items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+    {key: 10, item: 'Item 10'},
+    {key: 11, item: 'Item 11'},
+    {key: 12, item: 'Item 12'},
+    {key: 13, item: 'Item 13'},
+    {key: 14, item: 'Item 14'},
+    {key: 15, item: 'Item 15'},
+    {key: 16, item: 'Item 16'},
+    {key: 17, item: 'Item 17'},
+    {key: 18, item: 'Item 18'},
+    {key: 19, item: 'Item 19'},
+    {key: 20, item: 'Item 20'},
+  ])
+
+  const [Refreshing, SetRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    SetRefreshing(true);
+    setItems([...Items, {key: 21, item: 'Item 21'}])
+    SetRefreshing(false);
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>8</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>9</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>10</Text>
-        </View>
-      </View>
-    </View>
+    <ScrollView
+      style={styles.container}
+      refreshControl={ 
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['red']}
+        />
+      }
+    >
+      {
+        Items.map((element) => {
+          return (
+            <View style={styles.row} key={element.key}>
+              <Text style={styles.text}>{ element.item }</Text>
+            </View>
+          )
+        })
+      }
+      
+      </ScrollView>
   );
 }
 
@@ -53,66 +63,19 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+   
   },
   row: {
-    flexDirection: 'row',
-    backgroundColor: 'red',
-    alignItems: 'center',
+    marginTop: 30,
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    height: 100,
   },
+  text: {
+    fontSize: 25,
+  }
 
-   big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-  },
-   
- view1: {
-    flex: 0.5, // flex gives the ratio of the sapces among the items to fit the width or height of the
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
- view2: {
-    flex: 1,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },  
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#0fff0f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   view6: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  },
-    view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
      
 });
